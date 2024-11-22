1cfff2a9a21dffe2cf0ba6a8b563327440fd443e
pha-ran <pha-ran@naver.com>
2024-11-22 20:41:18 +0900

    Add session

---
+++ iocp_echo_serialization_buffer/session.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "ring_buffer.h"
+#include <winsock2.h>
+
+class session final
+{
+	friend class lan_server;
+
+private:
+#pragma warning(suppress:26495)
+	inline session(void) noexcept
+		: _socket(INVALID_SOCKET), _io_count(-1), _io_send(-1), _port(0)
+	{
+		_key._all = -1;
+		_ip[0] = L'\0';
+	}
+
+private:
+	SOCKET _socket;
+
+	union key
+	{
+		unsigned long long _all;
+		struct
+		{
+			unsigned long long _id : 48;
+			unsigned long long _index : 16;
+		};
+	} _key;
+
+	volatile LONG _io_count;
+	volatile CHAR _io_send;
+
+	unsigned short _port;
+	WCHAR _ip[16];
+
+	WSAOVERLAPPED _recv_overlapped;
+	WSAOVERLAPPED _send_overlapped;
+
+	ring_buffer _recv_buffer;
+	ring_buffer _send_buffer;
+};
